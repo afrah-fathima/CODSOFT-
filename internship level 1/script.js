7e7d3a4815072c6870ec85d7fb0d459140d53154
afrah-fathima <138352424+afrah-fathima@users.noreply.github.com>
2024-01-02 23:28:23 +0530

    Add files via upload

---
+++ internship level 1/script.js
@@ -0,0 +1,8 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('nav ul li a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', e => {
+            e.preventDefault();
+            const target = document.querySelector(link.getAttribute('href'));
+            target.scrollIntoView
